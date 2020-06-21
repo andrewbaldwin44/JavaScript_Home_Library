470d4ffa588c2cac32ebed0d5ea8e3090d7e7528
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-21 17:33:37 -0400

    Reformat library

--- bin/library.js
+++ bin/library.js
@@ -1,16 +1,16 @@
 const bookList = new BookList();
 
-bookList.add(new Book('The Shining', 'Horror', 'Stephen King'));
-bookList.add(new Book('American Gods', 'Fiction', 'Neil Gaiman'));
+bookList.add(new Book('The Shining', 'Stephen King', 'Horror'));
+bookList.add(new Book('American Gods', 'Neil Gaiman', 'Fiction'));
 bookList.add(
-  new Book('Eloquent JavaScript', 'Programming', 'Marijn Haverbeke', true)
+  new Book('Eloquent JavaScript', 'Marijn Haverbeke', 'Programming', true)
 );
-bookList.add(new Book('The Eire Affair', 'Fantasy', 'Jasper Fforde'));
+bookList.add(new Book('The Eire Affair', 'Jasper Fforde', 'Fantasy'));
 bookList.add(
-  new Book('The Revisionists', 'Science-fiction', 'thomas Mullen')
+  new Book('The Revisionists', 'Thomas Mullen', 'Science-fiction')
 );
-bookList.add(new Book('Harry Potter', 'Fantasy', 'J.K. Rowling', true));
-bookList.add(new Book('The Murder of Roger Ackroyd', 'Mystery', 'Agatha Christie', true));
-bookList.add(new Book('Death on the Nile', 'Mystery', 'Agatha Christie', false));
-bookList.add(new Book('A Fine Balance', 'Fiction', 'Rohinton Mistry', true));
-bookList.add(new Book('Cutting for Stone', 'Fiction', 'Abraham Verghese', false));
+bookList.add(new Book('Harry Potter', 'J.K. Rowling', 'Fantasy', true));
+bookList.add(new Book('The Murder of Roger Ackroyd', 'Agatha Christie', 'Mystery', true));
+bookList.add(new Book('Death on the Nile', 'Agatha Christie', 'Mystery', false));
+bookList.add(new Book('A Fine Balance', 'Rohinton Mistry', 'Fiction', true));
+bookList.add(new Book('Cutting for Stone', 'Abraham Verghese', 'Fiction', false));
